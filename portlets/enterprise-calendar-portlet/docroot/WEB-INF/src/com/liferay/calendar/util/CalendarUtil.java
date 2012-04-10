b3ae5d47c1eebcded86e9120f24917327ea4c451
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2012-04-10 15:08:10 -0300

    LPS-26069 Filter permissions using sublist (waiting resourceBlockId changes to replace)

--- portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarUtil.java
+++ portlets/enterprise-calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/util/CalendarUtil.java
@@ -14,13 +14,19 @@
 
 package com.liferay.calendar.util;
 
+import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.util.comparator.CalendarNameComparator;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.util.UniqueList;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author Eduardo Lundgren
@@ -28,6 +34,66 @@ import java.util.List;
  * @author Fabio Pezzutto
  */
 public class CalendarUtil {
+
+	public static final String AM_SUFFIX = "am";
+
+	public static final String HOUR_SUFFIX = "h";
+
+	public static final String PM_SUFFIX = "pm";
+
+	public static String formatHour(int isoHour) {
+		return formatHour(isoHour, true, true, true);
+	}
+
+	public static String formatHour(int isoHour, boolean isoFormat) {
+		return formatHour(isoHour, isoFormat, true, true);
+	}
+
+	public static String formatHour(
+		int isoHour, boolean isoFormat, boolean padHours) {
+
+		return formatHour(isoHour, isoFormat, padHours, true);
+	}
+
+	public static String formatHour(
+		int isoHour, boolean isoFormat, boolean padHours, boolean showSuffix) {
+
+		int hour = isoHour;
+		String formatted = StringPool.BLANK;
+		String suffix = StringPool.BLANK;
+
+		if (isoFormat) {
+			if (showSuffix) {
+				suffix = CalendarUtil.HOUR_SUFFIX;
+			}
+		}
+		else {
+			if (isoHour == 0) {
+				hour = 12;
+			}
+			else if (isoHour > 12) {
+				hour -= 12;
+			}
+
+			if (showSuffix) {
+				if (isoHour < 12) {
+					suffix = CalendarUtil.AM_SUFFIX;
+				}
+				else {
+					suffix = CalendarUtil.PM_SUFFIX;
+				}
+			}
+		}
+
+		if (padHours) {
+			formatted = String.format("%02d", hour);
+		}
+		else {
+			formatted = String.valueOf(hour);
+		}
+
+		return formatted.concat(suffix);
+	}
 
 	public static OrderByComparator getOrderByComparator(
 		String orderByCol, String orderByType) {
@@ -70,6 +136,27 @@ public class CalendarUtil {
 		}
 
 		return StringUtil.split(StringUtil.merge(keywordsList));
+	}
+
+	public static JSONObject toJSON(Calendar calendar, Locale locale) {
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put("name", calendar.getName(locale));
+		jsonObject.put("color", ColorUtil.toHexString(calendar.getColor()));
+
+		return jsonObject;
+	}
+
+	public static JSONArray toJSON(List<Calendar> calendars, Locale locale) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		if (calendars != null) {
+			for (Calendar calendar : calendars) {
+				jsonArray.put(toJSON(calendar, locale));
+			}
+		}
+
+		return jsonArray;
 	}
 
 }
