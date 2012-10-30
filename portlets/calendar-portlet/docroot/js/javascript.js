bf7e7f4f02622540cda456156a4c79fb161cc406
Djalma araújo <djalma.araujo@liferay.com>
2012-10-30 11:45:54 -0300

    LPS-30729 Improvement when iterating availableCalendars on event recorder template

--- portlets/calendar-portlet/docroot/js/javascript.js
+++ portlets/calendar-portlet/docroot/js/javascript.js
@@ -1448,7 +1448,9 @@ AUI.add(
 							schedulerEvent = instance;
 						}
 
-						var calendar = CalendarUtil.availableCalendars[schedulerEvent.get('calendarId')];
+						var availableCalendars = CalendarUtil.availableCalendars;
+
+						var calendar = availableCalendars[schedulerEvent.get('calendarId')];
 
 						var permissions = calendar.get('permissions');
 
@@ -1458,7 +1460,8 @@ AUI.add(
 								allDay: schedulerEvent.get('allDay'),
 								calendar: calendar,
 								editing: editing,
-								availableCalendars: instance._availableCalendarsToArray(),
+								calendarIds: AObject.keys(availableCalendars),
+								availableCalendars: availableCalendars,
 								permissions: permissions,
 								status: CalendarUtil.getStatusLabel(schedulerEvent.get('status'))
 							}
@@ -1476,12 +1479,6 @@ AUI.add(
 					},
 
 					isMasterBooking: Lang.emptyFnFalse,
-
-					_availableCalendarsToArray: function () {
-						return AObject.map(Liferay.CalendarUtil.availableCalendars, function(item, index, collection) {
-							return item;
-						});
-					},
 
 					_handleEventAcceptResponse: function(event) {
 						var instance = this;
